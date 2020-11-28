12cb5c5b013266554cecd950747e527ce8c7f00b
AlexanderMukas <alexandermukas@yahoo.com>
2020-11-29 01:53:47 +0200

    cleanup commit

--- routes/users.js
+++ routes/users.js
@@ -4,93 +4,11 @@ import { getUsers, createUser, getUser, updateUser, deleteUser } from '../contro
 
 const router = express.Router();
 
-// only array like 'database query'
-// const users = [
-//     {
-//         firstName: "John",
-//         lastName: "Doe",
-//         age: 25
-//     },
-//     {
-//         firstName: "Alex",
-//         lastName: "Dogson",
-//         age: 22
-//     }
-// ];
-// let users = [];
-
-// all routes in here are starting with /users
-
-// const getUsers = (req, res) => {
-//     res.send(users);
-// };
-
+// CRUD
 router.get('/', getUsers);
-
-// const createUser = (req, res) => {
-//     const user = req.body;
-//     const userWithId = { id: uuidv4(), ...user};
-    
-//     users.push(userWithId);
-
-//     res.send(`User with the name ${user.firstName} added to the DATABASE`);
-// }
-
-
-// CREATE (CRUD)
 router.post('/', createUser);
-
-
-// READ (CRUD)
-// /users/2 => req.params { id: 2}
-
-
-// const getUser = (req, res) => {
-//     // const id = req.params.id;
-//     const { id } = req.params;
-
-//     const foundUser = users.find( user => user.id === id);
-
-//     res.send(foundUser);
-// }
-
-
-
 router.get('/:id', getUser);
-
-// UPDATE (CRUD)
-
-
-// const updateUser = (req, res) => {
-//     const { id } = req.params;
-//     // from client form
-//     const { firstName, lastName, age } = req.body;
-
-//     const user = users.find( user => user.id === id);
-
-//     if(firstName){
-//         user.firstName = firstName;
-//     } else if(lastName){
-//         user.lastName = lastName;
-//     } else if(age){
-//         user.age = age;
-//     }
-    
-//     res.send(`User with id ${id} has been updated from the DATABASE`);
-
-// }
-
 router.patch('/:id', updateUser);
-
-// DELETE (CRUD)
-
-// const deleteUser = (req, res) => {
-//     const { id } = req.params;
-
-//     users = users.filter( user => user.id !== id);
-//     res.send(`User with id ${id} deleted from the DATABASE`);
-// }
-
 router.delete('/:id', deleteUser);
 
 export default router;
