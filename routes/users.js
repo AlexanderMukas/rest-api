4e4ee2a2d9dc0960efd29b0c11f00ab1197dfaae
AlexanderMukas <alexandermukas@yahoo.com>
2020-11-28 22:38:00 +0200

    add new route commit

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,29 @@ import express from 'express';
 
 const router = express.Router();
 
+const users = [
+    {
+    firstName: "John",
+    lastName: "Doe",
+    age: 25
+    },
+    {
+        firstName: "Alex",
+        lastName: "Dogson",
+        age: 22
+    }
+];
+
+
 // all routes in here are starting with /users
 router.get('/', (req, res) => {
-    res.send('hello users');
+    console.log(users);
+    res.send(users);
 });
+
+router.post('/', (req, res) => {
+    
+});
+
 
 export default router;
