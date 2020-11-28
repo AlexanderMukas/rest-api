0bfc86ee8b6a2ed5fa7f622ceece36cb022b86d2
AlexanderMukas <alexandermukas@yahoo.com>
2020-11-28 22:22:46 +0200

    create user.json file commit

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ import express from 'express';
 
 const router = express.Router();
 
+// all routes in here are starting with /users
 router.get('/', (req, res) => {
     res.send('hello users');
 });
