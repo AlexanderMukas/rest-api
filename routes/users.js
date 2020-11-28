70cfa64e3604376c81e4b0ebeeb8f11e0e348846
AlexanderMukas <alexandermukas@yahoo.com>
2020-11-28 23:41:07 +0200

    add read user from id commit

--- routes/users.js
+++ routes/users.js
@@ -18,19 +18,14 @@ const router = express.Router();
 // ];
 const users = [];
 
-
 // all routes in here are starting with /users
-router.get('/', (req, res) => {
-    // console.log(users);
 
+router.get('/', (req, res) => {
     res.send(users);
 });
 
 router.post('/', (req, res) => {
     const user = req.body;
-
-    // const userId = uuidv4();
-
     const userWithId = { id: uuidv4(), ...user};
     
     users.push(userWithId);
@@ -38,5 +33,17 @@ router.post('/', (req, res) => {
     res.send(`User with the name ${user.firstName} added to the DATABASE`);
 });
 
+
+// /users/2 => req.params { id: 2}
+
+router.get('/:id', (req, res) => {
+    // const id = req.params.id;
+    const { id } = req.params;
+
+    const foundUser = users.find( user => user.id === id);
+
+    res.send(foundUser);
+
+});
 
 export default router;
