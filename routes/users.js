9a76929c79f96b16650fa4705768f5a3204f52bc
AlexanderMukas <alexandermukas@yahoo.com>
2020-11-28 23:30:08 +0200

    add post request commit

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,41 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
-const users = [
-    {
-    firstName: "John",
-    lastName: "Doe",
-    age: 25
-    },
-    {
-        firstName: "Alex",
-        lastName: "Dogson",
-        age: 22
-    }
-];
+// only array like 'database query'
+// const users = [
+//     {
+//         firstName: "John",
+//         lastName: "Doe",
+//         age: 25
+//     },
+//     {
+//         firstName: "Alex",
+//         lastName: "Dogson",
+//         age: 22
+//     }
+// ];
+const users = [];
 
 
 // all routes in here are starting with /users
 router.get('/', (req, res) => {
-    console.log(users);
+    // console.log(users);
+
     res.send(users);
 });
 
 router.post('/', (req, res) => {
+    const user = req.body;
+
+    // const userId = uuidv4();
+
+    const userWithId = { id: uuidv4(), ...user};
     
+    users.push(userWithId);
+
+    res.send(`User with the name ${user.firstName} added to the DATABASE`);
 });
 
 
